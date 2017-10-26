9a9aa2b8c82b7e1331e255a17c519cb5d98d50ec
layssbp <laysbrito17@gmail.com>
2017-10-26 21:41:21 -0200

    test
    testando 123

--- sourceCode/view/Main.py
+++ sourceCode/view/Main.py
@@ -10,9 +10,19 @@ controller.carregarSalas();
 #test
 
 controller.alocarTurmasSalas("chp54125", "Microsoft");
-
-
+controller.buscaItem("Croquete");
+controller.buscaSala("1");
+controller.buscaEstudante(123);
+controller.modificarEstudante("chp12345",321,"Francis");
+controller.modificarItens("Pastel de Catupiri",2.50,10);
+controller.modificarSala(456,"20:10");
+controller.novoPedido(1,"Croquete");
+controller.novoRegistroEstudante("joao",123,"chp54125");
+controller.podeComprar("20:00");
+controller.novoProduto("Abel",1.55,20);
 
 root = Tk()
 Application(root)
 root.mainloop()
+
+controller.registrarLucro();
