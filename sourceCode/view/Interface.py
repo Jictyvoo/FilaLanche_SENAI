bf7081e96df281658425d6da6c92006228c836d9
João Victor Oliveira <jictyvoo.ecomp@gmail.com>
2017-10-27 20:06:44 -0200

    Iniciada nova interface

---
+++ sourceCode/view/Interface.py
@@ -0,0 +1,40 @@
+from sourceCode.controller.MainController import MainController
+
+class Interface:
+    def __init__(self):
+        self.controller = MainController()
+        self.controller.carregarProdutosEmEstoque()
+        self.controller.carregarSalas()
+
+    def inserirIdEstudante(self):
+        print("Insira o ID do Estudante:")
+        idEstudante = str(input())
+        return idEstudante
+
+    def mainLoop(self):
+        idEstudante = self.inserirIdEstudante()
+        estudante = self.controller.buscaEstudante(idEstudante)
+        if(not estudante):
+            print("Estudante nao encontrado!")
+            print("[1] - Inserir novo estudante")
+            selecao = input()
+            if(selecao == "1"):
+                print("ola")
+            else:
+                self.mainLoop()
+
+    def realizarCadastros(self):
+        self.controller.alocarTurmasSalas("chp54125", "Microsoft")
+        self.controller.buscaItem("Croquete")
+        self.controller.buscaSala("1")
+        self.controller.buscaEstudante(123)
+        self.controller.modificarEstudante("chp12345", 321, "Francis")
+        self.controller.modificarItens("Pastel de Catupiri", 2.50, 10)
+        self.controller.modificarSala(456, "20:10")
+        self.controller.novoPedido(1, "Croquete")
+        self.controller.novoRegistroEstudante("joao", 123, "chp54125")
+        self.controller.podeComprar("20:00")
+        self.controller.novoProduto("Abel", 1.55, 20)
+
+    def getController(self):
+        return self.controller
--- sourceCode/view/Main.py
+++ sourceCode/view/Main.py
@@ -2,29 +2,14 @@
 
 
 from tkinter import *
-from sourceCode.controller.MainController import MainController;
-from sourceCode.view.Graphics import Application;
+from sourceCode.view.Interface import Interface
+from sourceCode.view.Graphics import Application
 
-controller = MainController();
-controller.carregarProdutosEmEstoque();
-controller.carregarSalas();
-
-# test
-
-controller.alocarTurmasSalas("chp54125", "Microsoft");
-controller.buscaItem("Croquete");
-controller.buscaSala("1");
-controller.buscaEstudante(123);
-controller.modificarEstudante("chp12345", 321, "Francis");
-controller.modificarItens("Pastel de Catupiri", 2.50, 10);
-controller.modificarSala(456, "20:10");
-controller.novoPedido(1, "Croquete");
-controller.novoRegistroEstudante("joao", 123, "chp54125");
-controller.podeComprar("20:00");
-controller.novoProduto("Abel", 1.55, 20);
+interface = Interface()
+interface.mainLoop()
 
 root = Tk()
 Application(root)
 root.mainloop()
 
-controller.registrarLucro();
+interface.getController().registrarLucro()
