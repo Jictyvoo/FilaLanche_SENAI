91c6afe44af8a754d12c4adaa7c48a025b4b01fd
argalvao <32915303+argalvao@users.noreply.github.com>
2017-10-27 21:07:15 -0300

    Interface gráfica
    O básico da interface

---
+++ sourceCode/view/Interface.py
@@ -0,0 +1,99 @@
+from sourceCode.controller.MainController import MainController
+import os
+
+
+class Interface:
+    def __init__(self):
+        self.controller = MainController()
+        self.controller.carregarProdutosEmEstoque()
+        self.controller.carregarSalas()
+
+    def inserirIdEstudante(self):
+        print("Insira o ID do Estudante:")
+        idEstudante = str(input())
+        return idEstudante
+
+    def cadastrarEstudante(self):
+        turmaEstudante = ""
+        idEstudante = 0
+        nomeEstudante = ""
+        wrongInput = True
+        while (wrongInput):
+            try:
+                os.system("cls || clear")
+                print("Insira o ID do Estudante")
+                idEstudante = int(input())
+                wrongInput = False
+            except(ValueError):
+                wrongInput = True
+        wrongInput = True
+        while (wrongInput):
+            try:
+                os.system("cls || clear")
+                print("Insira a turma do Estudante")
+                turmaEstudante = str(input())
+                wrongInput = False
+            except():
+                wrongInput = True
+        wrongInput = True
+        while (wrongInput):
+            try:
+                os.system("cls || clear")
+                print("Insira o nome do Estudante")
+                nomeEstudante = str(input())
+                wrongInput = False
+            except():
+                wrongInput = True
+        self.controller.novoEstudante(nomeEstudante, idEstudante, turmaEstudante)
+        os.system("cls || clear")
+        print("Cadastro realizado com sucesso!")
+        return idEstudante
+
+    def realizarPedido(self, idEstudante):
+        for index, value in enumerate(self.controller.getTodosProdutos()):
+            print("[" + str(index) + "] - " + value.getNome() + " -- R$" + str(value.getPreco()))
+        print("[] - Sair\nOBS: INSIRA OS PRODUTOS QUE DESEJA COMPRAR DA SEGUINTE FORMA: id*qtd-id*qtd")
+        entrada = input()
+
+        analisa = str(entrada).split("-")
+        pedido = []
+        for index, value in enumerate(analisa):
+            divide = value.split("*")
+            for position in range(0, int(divide[1])):
+                pedido.append(self.controller.getTodosProdutos()[int(divide[0])])
+        if (not self.controller.novoPedido(idEstudante, pedido)):
+            print("Erro! Turma nao alocada em sala")
+
+    def mainLoop(self):
+        idEstudante = self.inserirIdEstudante()
+        if (idEstudante == ""):
+            return False
+        estudante = self.controller.buscaEstudante(idEstudante)
+        if (not estudante):
+            print("Estudante nao encontrado!")
+            print("[1] - Inserir novo estudante\n[] - Buscar novamente")
+            selecao = input()
+            if (selecao == "1"):
+                idEstudante = self.cadastrarEstudante()
+                self.realizarPedido(idEstudante)
+            else:
+                self.mainLoop()
+        else:
+            print("[1] - Modificar Estudante\n[2] - Realizar Pedido\n[] - Sair")
+            entrada = input()
+            if (entrada == "1"):
+                self.controller.modificarEstudante("chp12345", 321, "Francis")
+            elif (entrada == "2"):
+                self.realizarPedido(idEstudante)
+            else:
+                self.mainLoop()
+        return True
+
+    def realizarCadastros(self):
+        self.controller.alocarTurmasSalas("chp54125", "Microsoft")
+        self.controller.modificarItens("Pastel de Catupiri", 2.50, 10)
+        self.controller.modificarSala(456, "20:10")
+        self.controller.novoProduto("Abel", 1.55, 20)
+
+    def getController(self):
+        return self.controller
--- sourceCode/view/Main.py
+++ sourceCode/view/Main.py
@@ -2,29 +2,91 @@
 
 
 from tkinter import *
+<<<<<<< HEAD
 from sourceCode.controller.MainController import MainController;
 from sourceCode.view.Graphics import Application;
 
-controller = MainController();
-controller.carregarProdutosEmEstoque();
-controller.carregarSalas();
+main = MainController()
 
-# test
+class Main:
+    def __init__(self, master=None):
+        self.fontePadrao = ("Arial", "10")
+        self.c1 = Frame(master)
+        self.c1["pady"] = 10
+        self.c1.pack()
 
-controller.alocarTurmasSalas("chp54125", "Microsoft");
-controller.buscaItem("Croquete");
-controller.buscaSala("1");
-controller.buscaEstudante(123);
-controller.modificarEstudante("chp12345", 321, "Francis");
-controller.modificarItens("Pastel de Catupiri", 2.50, 10);
-controller.modificarSala(456, "20:10");
-controller.novoPedido(1, "Croquete");
-controller.novoRegistroEstudante("joao", 123, "chp54125");
-controller.podeComprar("20:00");
-controller.novoProduto("Abel", 1.55, 20);
+        self.c2 = Frame(master)
+        self.c2["padx"] = 20
+        self.c2.pack()
+
+        self.c3 = Frame(master)
+        self.c3["padx"] = 20
+        self.c3.pack()
+
+        self.c4 = Frame(master)
+        self.c4["pady"] = 20
+        self.c4.pack()
+
+        self.titulo = Label(self.c1, text="Cantina SENAI")
+        self.titulo["font"] = ("Arial", "10", "bold")
+        self.titulo.pack()
+
+        self.idAluno = Label(self.c2, text="      ID", font=self.fontePadrao)
+        self.idAluno.pack(side=LEFT)
+
+        self.turma = Entry(self.c2)
+        self.turma["width"] = 30
+        self.turma["font"] = self.fontePadrao
+        self.turma.pack(side=LEFT)
+
+
+        self.autenticar = Button(self.c4)
+        self.autenticar["text"] = "Autenticar"
+        self.autenticar["font"] = ("Calibri", "8")
+        self.autenticar["width"] = 12
+        self.autenticar["command"] = self.verificaAluno
+        self.autenticar.pack()
+
+        self.mensagem = Label(self.c4, text="", font=self.fontePadrao)
+        self.mensagem.pack()
+
+    def verificaAluno(self):
+        idAluno = self.turma.get()
+        if idAluno == main.buscaEstudante(idAluno):
+            self.mensagem["text"] = "Autenticado"
+        else:
+            self.mensagem["text"] = "Erro na autenticação"
+
 
 root = Tk()
-Application(root)
+Main(root)
 root.mainloop()
+=======
+from sourceCode.view.Interface import Interface
+import os
+import datetime
 
-controller.registrarLucro();
+interface = Interface()
+while (interface.mainLoop()):
+    os.system("cls")
+
+
+# root = Tk()
+# Application(root)
+# root.mainloop()
+
+
+def mensagem():
+    currentTime = datetime.datetime.now()
+    if (currentTime.hour < 12):
+        print("Bom dia!")
+    elif 12 <= currentTime.hour < 18:
+        print("Boa tarde!")
+    else:
+        print("Boa noite!")
+
+
+mensagem()
+print("Seja bem vindo ao sistema de cantina do SENAI!")
+print("Para iniciar coloque a sua matrícula!")
+>>>>>>> b9be4afa2b602367bdc8a9bb6853217f2bc80028
