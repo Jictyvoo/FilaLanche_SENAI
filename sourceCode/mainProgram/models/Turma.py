d4c621500dc06c37a4331e047af42e0625bb0127
argalvao <32915303+argalvao@users.noreply.github.com>
2017-11-29 19:53:15 -0300

    Classe Turma
    Classe da turma implementada

--- sourceCode/mainProgram/models/Turma.py
+++ sourceCode/mainProgram/models/Turma.py
@@ -4,3 +4,15 @@ from sourceCode.mainProgram.models.DatabaseManipulator import DatabaseManipulato
 class Turma(DatabaseManipulator):
     def __init__(self, conexao):
         super(Turma, self).__init__(conexao)
+
+    def getConexao(self):
+        return self.__conexao
+
+    def getNome(self, id_turma):
+        self.__cursor.execute('select nome from Produto where id_pessoa = "%d"' % id_turma)
+        nome = self.__cursor.fetchone()
+        return nome[0]
+
+    def setNome(self, id_turma, nome):
+        self.__cursor.execute('update Produto set nome = "%s" where id_pessoa = "%d"' % nome, id_turma)
+        self.__conexao.commit()
